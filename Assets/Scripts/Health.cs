e61b3a9a3d52ee72e05c9d85d9b0250150648faf
NuhitoAtmoko <atmokonuhito@gmail.com>
2024-06-17 12:07:30 +0700

    finishing up -ito

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -5,18 +5,27 @@ using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
-    public int health = 5;
+    public static int health = 5;
 
     public Image[] hearts;
-    // Start is called before the first frame update
-    void Start()
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
+    void Awake()
     {
-        
+        health = 5;
     }
 
 
     void Update()
     {
-        
+        foreach(Image img in hearts)
+        {
+            img.sprite = emptyHeart;
+        }
+        for(int i = 0; i < health; i++)
+        {
+            hearts[i].sprite = fullHeart;
+        }
     }
 }
